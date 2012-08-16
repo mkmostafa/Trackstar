2c0fafad0b2983099085afc395d365dd25ad9ae6
cena <mk.cena92@gmail.com>
2012-08-17 00:23:32 +0200

    update

--- trackstar/protected/config/test.php
+++ trackstar/protected/config/test.php
@@ -4,14 +4,17 @@ return CMap::mergeArray(
 	require(dirname(__FILE__).'/main.php'),
 	array(
 		'components'=>array(
+
+
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
-			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
+			                 ),
+
+			'db'=>array( 'connectionString' =>'mysql:host=127.0.0.1;dbname=trackstar_test',
+			              'emulatePrepare' => true,
+			              'username' => 'root',
+			              'charset' => 'utf8',
+			              ),
 		),
 	)
 );
--- trackstar/protected/tests/unit/ProjectTest.php
+++ trackstar/protected/tests/unit/ProjectTest.php
@@ -1,6 +1,10 @@
 <?php
 class ProjectTest extends CDbTestCase
 {
+	public $fixtures=array(
+		'projects'=>'Project',
+		);
+
 	public function testCrud()
 	{
 		//Creating new instance test.
@@ -24,5 +28,51 @@ class ProjectTest extends CDbTestCase
 		$this-> assertTrue($retrievedProject instanceof Project);
 		$this-> assertEquals($retrievedProject->name, $newProjectName);
 
+		//Updating the created instance
+		$updatedProjectName = 'Updated Project 1';
+		$newProject->name = $updatedProjectName;
+		$this->assertTrue($newProject->save(false));
+
+		//Deleting the created instance
+		$projectId = $newProject->id;
+		$this->assertTrue($newProject->delete());
+		$deletedProject = Project::model()->findByPk($projectId);
+		$this->assertEquals(NULL,$deletedProject);
+
+	}
+
+	public function testRead()
+	{
+		$retrievedProject = $this->projects('project1');
+		$this->assertEquals($retrievedProject->name,'Test Project 1');
+	}
+
+	public function testDelete()
+	{
+		$projectId = $this->projects('project2')->id;
+		$this->assertTrue($this->projects('project2')->delete());
+		$retrievedProject = Project::model()->findByPk($projectId);
+		$this->assertEquals(NULL,$retrievedProject);
+	}
+
+	public function testUpdate()
+	{
+		$project = $this->projects('project1');
+		$project->name = 'Updated Project 1';
+		$this->assertTrue($project->save(false));
+		$this->assertEquals($this->projects('project1')->name,'Updated Project 1');
+	}
+	public function testCreate()
+	{
+		$project = new Project;
+		$project -> setAttributes(
+			array (
+				'name'=>'created test project 1',
+				'description'=>'created test project 1',
+				)
+			);
+		$this -> assertTrue($project->save(false));
+		$retrievedProject = Project::model()->findByPk($project->id);
+		$this->assertEquals($retrievedProject->name, 'created test project 1');
 	}
 }
